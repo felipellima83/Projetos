34378ec2af97a0b66f68fc6faf2cd9bfe71a7477
Felipe Lima <felipellima83@gmail.com>
2020-05-06 10:28:09 -0300

    Update I07e09comparar_valores.py

--- UniCEUB/Semestre 01/LogicaProgramacao/Aula 05.06.2020/I07e09comparar_valores.py
+++ UniCEUB/Semestre 01/LogicaProgramacao/Aula 05.06.2020/I07e09comparar_valores.py
@@ -7,5 +7,16 @@ def compara_valores (a,b):
         ret = -1
     return ret
 
+def compara_valores2 (a,b):
+    if a > b:
+        print("Resposta: Primeiro maior")
+    elif a < b:
+        print("Resposta: Segundo maior")
+    else:
+        print("Resposta: Iguais")
+
 if __name__ == "__main__":
     print(compara_valores(float(input("Qual número? ")),float(input("Qual número? "))))
+    n1 = float(input("Qual número? "))
+    n2 = float(input("Qual número? "))
+    compara_valores2(n1,n2)
