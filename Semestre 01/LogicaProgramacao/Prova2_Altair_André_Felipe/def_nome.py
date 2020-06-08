a99b7e96acccda4049619911897389dec85f8883
Felipe Lima <felipellima83@gmail.com>
2020-06-08 11:12:46 -0300

    ajuste

---
+++ Semestre 01/LogicaProgramacao/Prova2_Altair_André_Felipe/def_nome.py
@@ -0,0 +1,15 @@
+def senha (a):
+    senha = int(input("Digite a senha: "))
+    while senha != 123:
+        print("Senha incorreta. Tente novamente!")
+        senha = int(input("Digite a senha: "))
+    print("Acesso Garantido!")
+    return senha
+
+def grafico (a,b,c,d,e):
+    
+        
+if __name__ == "__main__":
+    print("programa iniciado")
+    senha (senha)
+    
