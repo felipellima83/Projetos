3a47984dc2b360ae548f34fb27d28fa090e4e489
Felipe Lima <felipellima83@gmail.com>
2020-09-10 11:54:58 -0300

    Update crud.py

--- Semestre 02/ProjetoIntegrador2/Aula 09.03.2020/crud.py
+++ Semestre 02/ProjetoIntegrador2/Aula 09.03.2020/crud.py
@@ -1,57 +1,13 @@
+# Exercício de CRUD
 
-
-def menu():
-    l_opcoes = ['c','r','u','d','e']
-    print("[c] - create")
-    print("[r] - read")
-    print("[u] - update")
-    print("[d] - delete")
-    print("[e] - exit")
-    opcao = input('Opção: ')
+def menu():    
     while True:
-        opcao = input('Opção: ').lower()
-        if opcao in l_opcoes:
-            break            
-        else:
-            print("Opção inválida, tente novamente.")
-    return opcao
-
-def create():
-    n = input("Nome: ")
-    lista.append(n)
-    
-def read():
-    if lista != []:
-        ct = 0
-        for i in lista:
-            print(ct, '-',i)
-            ct+=1
-    else:
-        print("Lista vazia.")           
-        
-def update():
-    if lista != []:
-        read()
-        p = int(input("Qual posição: "))
-        novo_nome = input("Novo nome: ")
-        try:
-            lista[p] = novo_nome
-        except IndexError as e:
-            print("Erro IndexError: ",e)
-        except Exception as e:
-            print("Erro Exception: ",e)
-    else:
-        print("lista vazia.")
-
-
-def delete():
-    v = input("Qual nnome: ")
-    lista.remove(v)
-
-if __name__ == "__main__":
-    lista = []
-    while True:
-        op = menu()
+        print("[c] - Create")
+        print("[r] - Read")
+        print("[u] - Update")
+        print("[d] - Delete")
+        print("[e] - Exit")
+        op = input('Opção: ').lower()
         if op == 'c':
             create()
         elif op == 'r':
@@ -60,5 +16,87 @@ if __name__ == "__main__":
             update()
         elif op == 'd':
             delete()
-        else:
+        elif op == 'e':
             break
+        else:
+            print("Opção inválida, tente novamente!")
+
+def create():
+    op_create = ['p','f','s']
+    while True:
+        print("[p] - Incluir um ou mais nomes ao final da lista.")
+        print("[f] - Incluir um ou mais nomes em posições específicas da lista.")
+        print("[s] - Sair.")
+        op_create = input("Qual opção: ").lower()
+        if op_create == 'p':
+            n = input(" Digite o nome: ")
+            lista.append(n)            
+        elif op_create == 'f':
+            n = input(" Digite o nome: ")
+            try:
+                p = int(input("Qual posição na lista: "))
+                lista.insert(p,n)
+            except IndexError as e:
+                print("Erro IndexError: ",e)
+            except Exception as e:
+                print("Erro Exception: ",e)
+        elif op_create == 's':
+            break
+        else:
+            print("Opção inválida, tente novamente.")    
+    
+def read():
+    if lista_nao_vazia():
+        ct = 0
+        for i in lista:
+            print(ct, '-',i)
+            ct+=1        
+        
+def update():
+    while True:
+        if lista_nao_vazia():
+            read()
+            print("[p] - Deseja atualizar pela posição na lista: ")
+            print("[n] - Deseja atualizar pelo nome na lista: ")
+            print("[s] - Sair")
+            op_update = input("Qual opção: ").lower()
+            if op_update == 'p':
+                try:
+                    p = int(input("Qual posição: "))
+                    novo_nome = input("Novo nome: ")
+                    lista[p] = novo_nome
+                except IndexError as e:
+                    print("Erro IndexError: ",e)
+                except Exception as e:
+                    print("Erro Exception: ",e)
+            elif op_update == 'n':
+                nome_lista = input("nome da lista: ")
+                if nome_lista in lista:
+                    i = lista.index(nome_lista)
+                    novo_nome = input("Novo nome: ")        
+                    lista[i] = novo_nome
+            elif op_update == 's':
+                break
+            else:
+                print("Opção inválida, tente novamente.")
+
+def delete():
+    if lista_nao_vazia():
+        read()
+        v = input("Qual nome deseja remover? ")
+        if v in lista:
+            lista.remove(v)            
+        else:
+            print("Esse nome {} não existe na lista!".format(v))
+
+def lista_nao_vazia():
+    if lista != []:
+        nao_vazia = True
+    else:
+        nao_vazia = False
+        print("Lista vazia.")
+    return nao_vazia
+
+if __name__ == "__main__":
+    lista = []
+    menu()
