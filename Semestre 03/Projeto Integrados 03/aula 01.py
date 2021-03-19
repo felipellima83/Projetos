b52d0f383b1798c1fd9aa37e718d2b1498ab99df
Felipe Lima <felipellima83@gmail.com>
2021-03-19 18:23:57 -0300

    Update aula 01.py

--- Semestre 03/Projeto Integrados 03/aula 01.py
+++ Semestre 03/Projeto Integrados 03/aula 01.py
@@ -5,51 +5,86 @@ Atalho de teclado: ctlr <d>, duplica linha. ctrl <y>, apaga linha. ctrl </>, com
 Quando declaramos as variáveis na classe Conta, aprendemos que podemos atribuir um valor padrão
 para cada uma delas. Então, atribuo um valor padrão ao limite, por exemplo, 1000.0 reais.
 
-Implemente:
-1- Crie a classe Conta com os atributos numero, nome_cliente, saldo, limite.
+- Agregação
+Aumente a classe Conta, adicione os atributos nome, sobrenome e cpf do titular da conta.
+Desvantagem:
+Ficamos como muitos atributos e uma Conta não tem os atributos nome, sobrenome e cpf,
+Mas, esses atributos são de Cliente e não da Conta
+Assim, criamos uma nova classe e fazemos uma agregação, ou seja,
+agregar um cliente a conta.
+Portanto, a classe Conta tem um Cliente.
+Os atributos de uma Conta também podem ser referência (objeto) para outra classe.
+
+- Implemente:
+1- Crie a classe Cliente com os atributos nome, sobrenome e cpf.
 - Crie os métodos gets e sets.
-- Crie um objeto da classe Conta passando os dados
+- Crie um objeto da classe Cliente, teste.
+- Crie o método nome_completo
+5- Em Conta, altere o atributo nome_cliente (string) para cliente, ele vai receber um objeto cliente.
+- Crie um objeto da classe Conta usando o objeto cliente criado
+- Mostre o endereço do objeto cliente criado
 - Mostre o endereço do objeto conta criado
-5- Consulte e mostre os dados da conta
-6- Altere o nome do cliente do objeto Conta, teste.
-- Faça um depósito, teste.
-8- Faça um saque, teste.
-- Faça uma transferência, teste.
-10- Consulte um extrato com os dados: nome, número e saldo da conta
+- Mostre o endereço do objeto cliente criado, usando o objeto conta.
+10- Mostre o nome, sobrenome e cpf usando o objeto cliente
+- Altere o nome do objeto cliente, teste.
+- Mostre o nome, sobrenome e cpf usando o objeto conta.
+- Altere o nome do objeto cliente usando o objeto conta, teste.
+- Consulte o extrato com os dados: número e saldo da conta
+15- Consulte o extrato com os dados: nome, sobrenome, cpf, número e saldo da conta
+- Mostre todos os dados do atributo titular da classe Conta
 - Use o método __class__ no objeto cliente
 - Use o método __class__.__name__ no objeto cliente
 - Mostre os atributos do objeto cliente com o método __dict__
-14- Mostre os atributos do objeto cliente com o método vars           """
-class Cliente(object):
+- Mostre os atributos do objeto cliente com o método vars  """
+class Cliente(object):          # class Cliente:
     def __init__(self, nome, sobrenome, cpf):
         self.nome = nome
         self.sobrenome = sobrenome
         self.cpf = cpf
     def get_nome(self):
         return self.nome
-    def get_sobenome(self):
+    def set_nome(self, novo_nome):
+        self.nome = novo_nome
+    def get_sobrenome(self):
         return self.sobrenome
     def get_cpf(self):
         return self.cpf
     def nome_completo(self):
-        nc = f'{self.nome}, {self.sobrenome}'
+        nc = f'{self.nome} {self.sobrenome}'
         return nc
-class Conta:
-    def __init__(self, numero, Cliente, saldo, limite=1000.0):
+class Historico:
+    def __init__(self):
+        self.data_abertura = datetime.today()
+        self.transacoes = []
+    def get_data_abertura(self)
+class Conta:                    # class Conta(object):
+    def __init__(self, numero, cliente, saldo, limite=1000.0):  # O parãmetro cliente recebe um objeto da classe Cliente
         self.numero = numero
-        self.titular = Cliente
+        self.titular = cliente  # O atributo self.titular recebe o endereço do objeto cliente1
         self.saldo = saldo
         self.limite = limite
     def get_titular(self):
         return self.titular
-    def set_titular(self, nome):
-        self.titular = nome
+    def get_titular_nome(self):             # Consulta nome do cliente
+        return self.titular.nome
+    def set_titular_nome(self, novo_nome):  # Altera nome
+        self.titular.nome = novo_nome
+    def get_titular_sobrenome(self):
+        return self.titular.sobrenome
+    def get_titular_cpf(self):
+        return self.titular.cpf
     def get_saldo(self):
         return self.saldo
-    def get_numero(self):
-        return self.numero
+    def extrato(self):
+        print(f"Extrato 1:\nNúmero: {self.numero}, Saldo: {self.saldo}")
+        # print("Extrato 1:\nNúmero: {}, Saldo: {}".format(self.numero, self.saldo))
+    def extrato_2(self):
+        print(f'Extrato 2:\nNome: {self.titular.nome} {self.titular.sobrenome}, CPF: {self.titular.cpf}')
+        print("Número: {}, Saldo: {}".format(self.numero, self.saldo))
+    def dados_titular(self):
+        return self.titular.__dict__
     def deposito(self, valor):
-        self.saldo += valor             # self.saldo = self.saldo + valor
+        self.saldo += valor
     def saque(self, valor):
         if self.saldo + self.limite < valor:
             print('Saldo insuficiente.')
@@ -67,31 +102,31 @@ class Conta:
             destino.deposito(valor)
             print('Transferência realizada com sucesso')
             return True
-    def extrato(self):
-        # print("Extrato:\nNome: {}, Número: {}, Saldo: {}".format(self.titular, self.numero, self.saldo))
-        print(f"Extrato:\nNome: {self.titular}, Número: {self.numero}, Saldo: {self.saldo}")
+
 if __name__ == '__main__':  # mai <tab>
-    cliente1 = Cliente('Felipe', 'Lima', '70845093134')
-    print('Nome: ', cliente1.get_nome())
-    conta1 = Conta('123-4', cliente1, 1200.0, 1000.0)  # Chama o contrutor (__init__)
-    print(cliente1)
-    print(conta1)    # <conta_agregacao.Conta object at 0x000002B9DBA4AF70>
-    print('Nome:', conta1.get_titular())    # Consulta      nome_objeto.nome_metodo()
-    print('Número:', conta1.get_numero())
-    conta1.set_titular('Ana')               # Altera o nome do cliente
-    print('Nome:', conta1.get_titular())
-    conta1.deposito(200)
-    print('Saldo:', conta1.get_saldo())
-    conta1.saque(100)
-    print('Saldo:', conta1.get_saldo())
-    conta2 = Conta('923-4', 'Ailton', 2000.0, 1000.0)  # Chama o contrutor (__init__)
-    print(conta1)
-    conta1.transfere_para(conta2, 200)      # Transferência da conta1 para a conta2
-    conta1.transfere_para(conta2, 4000)     # Transferência da conta1 para a conta2
+    cliente1 = Cliente('João', 'Oliveira', '1111111111-1')  # Cria objeto da classe Cliente
+    print('Nome:', cliente1.get_nome())
+    conta1 = Conta('123-4', cliente1, 1200.0, 1000.0)       # Cria objeto da classe Conta usando o objeto da classe Cliente
+    print(cliente1)              # <conta_agregacao.Cliente object at 0x000002B9DBA4AFD0>, o endereço do objeto cliente1
+    print(conta1)                # <conta_agregacao.Conta object at 0x000002B9DBA4AF70>
+    print(conta1.get_titular())  # <conta_agregacao.Cliente object at 0x000002B9DBA4AFD0>, o endereço do atributo titular da classe Conta
+    print('Nome:', cliente1.get_nome())                     # Consulta usando a classe Cliente
+    print('Sobrenome:', cliente1.get_sobrenome())
+    print('CPF:', cliente1.get_cpf())
+    cliente1.set_nome('Ana')                                # Altera o nome usando a classe Cliente
+    print('Nome:', cliente1.get_nome())
+    print('Nome completo:', cliente1.nome_completo())
+    print('Nome:', conta1.get_titular_nome())               # Consulta usando a classe Conta
+    print('Sobrenome:', conta1.get_titular_sobrenome())
+    print('CPF:', conta1.get_titular_cpf())
+    conta1.set_titular_nome('Alice')                        # Altera o nome usando a classe Conta
+    print('Nome:', conta1.get_titular_nome())
     conta1.extrato()
-    conta2.extrato()
-    print('Método especiais:')
-    print(conta1.__class__)             # <class '__nain__.Conta'>
-    print(conta1.__class__.__name__)    # Conta
-    print(conta1.__dict__)              # {'numero': '123-4', 'titular': 'Ana', 'saldo': 1100.0, 'limite': 1000.0}
-    print(vars(conta1))                 # {'numero': '123-4', 'titular': 'Ana', 'saldo': 1100.0, 'limite': 1000.0}
+    conta1.extrato_2()
+    print('conta1.dados_titular():', conta1.dados_titular())
+    print(conta1.get_titular())             # Retorna o endereço
+    print('special method or dunder:')
+    print(cliente1.__class__)
+    print(cliente1.__class__.__name__)
+    print(cliente1.__dict__)                # Métodos semelhantes
+    print(vars(cliente1))
